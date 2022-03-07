f16cf65fbd22801f5746461ff87470b2f7fe0273
amberdevpe <amberdevpe@gmail.com>
2022-03-07 14:33:59 -0500

    Initial commit

---
+++ src/main/java/com/example/demojsp2/Demojsp2Application.java
@@ -0,0 +1,13 @@
+package com.example.demojsp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demojsp2Application {
+
+public static void main(String[] args) {
+    SpringApplication.run(Demojsp2Application.class, args);
+}
+
+}
